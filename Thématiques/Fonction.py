25d69a4e32e054ead0c2763a44485a1d158593a1
Orbital-Builder <mb35.bn@gmail.com>
2025-02-10 15:59:41 +0100

    Commit 1.1

---
+++ Thématiques/Fonction.py
@@ -0,0 +1,23 @@
+'''
+Les fonctions en python
+Les fonctions sont des blocs de code qui effectuent une tâche spécifique.
+Une fois que vous avez défini une fonction, vous pouvez l'appeler autant de fois que vous le souhaitez.
+Vous pouvez passer des données, appelées paramètres, à une fonction.
+Une fonction peut renvoyer des données en sortie.
+Les fonctions rendent votre code plus lisible et plus facile à gérer.
+Les fonctions peuvent être appelées à partir d'autres fonctions.
+Les fonctions sont utilisées pour organiser les programmes en groupes de code réutilisables.
+Les fonctions sont définies en utilisant le mot-clé def suivi du nom de la fonction et des parenthèses.
+Exemple:
+'''
+
+#Variables prenom et nom, dans lesquelles l'utilisateur entre son prénom et son nom, est la fonction input.
+prenom = input("Entrez votre prénom: ")
+nom = input("Entrez votre nom: ")
+print("Bonjour", prenom, " " + nom, "Bienvenue sur le panel administrateur cette espace et réservé aux administrateurs.")
+
+#Type de variable prenom,
+#Type affiche le type de variable de la variable donnée.
+print(type(prenom))
+
+
---
+++ Thématiques/Operateur_Mathematique.py
@@ -0,0 +1,15 @@
+'''
+Opérations mathématiques en python, comment cela fonctionne ?
+Les opérations mathématiques de base en python sont les suivantes: +, -, *, /, //, %, **.
+Comment le mettre en place ?
+'''
+
+a = 50
+x = 50
+print (a + x) #Addition
+print (a - x) #Soustraction
+print (a * x) #Multiplications
+print (a % x) #Modulo
+print (a ** x) #Exponentiation
+print (a // x) #Division entière
+print (a / x) #Division
